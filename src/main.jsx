eb7540f909bbc721a762494a3fef07c94e393a03
Johndillerman <U2486892@rts-365.dk>
2025-05-08 08:51:21 +0200

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router'
 import Index from './pages/Index'
-import Projects from './pages/projects'
+import Projects from './pages/Projects';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
       <Route path="/" element={<Index />} />
-      <Route path="/projects" element={<Projects />} />
+      <Route path="/Projects" element={<Projects />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
